31e8a5100abf77ca31e7c25f787f764d232e5f2a
apache2046 <apache2046@gmail.com>
2022-11-18 17:16:56 +0800

    1

--- controller2d_stanley.py
+++ controller2d_stanley.py
@@ -43,7 +43,7 @@ class Controller2D(object):
         self._pi                 = np.pi
         self._2pi                = 2.0 * np.pi
 
-        self.v_err_i = deque(maxlen = 30)
+        self.v_err_i = deque(maxlen = 10)
 
     def update_values(self, x, y, yaw, speed, timestamp, frame):
         self._current_x         = x
@@ -181,30 +181,24 @@ class Controller2D(object):
                 access the persistent variables declared above here. For
                 example, can treat self.vars.v_previous like a "global variable".
             """
-            dv = v - self.vars.v_previous
             v_err = v_desired - v
-            v_err_d = dv
-            #v_err_d = v_err - self.vars.v_err_previous
-            self.v_err_i.append(np.clip(v_err, -0.5, 0.5))
-            v_err_i = min(8, sum(self.v_err_i))
-            #v_err_d = np.clip(v_err_d, -0.05, 0.05)
+            v_err_d = v_err - self.vars.v_err_previous
+
+            # self.v_err_i = deque(maxlen = 10)
+            self.v_err_i.append(np.clip(v_err, -0.2, 0.2))
+            v_err_i = sum(self.v_err_i)
+
             Kp = 0.6
-            Ki = 0.01
-            Kd = -1.4
+            Ki = 0.1
+            Kd = +0.3
             
             acc_delta = Kp * v_err + Ki * v_err_i  + Kd * v_err_d
-            #print(f"{acc_delta:.03f}, {Kp * v_err:.03f}, {Ki * v_err_i:.03f}, {Kd * v_err_d:.03f}, {v_err_d:.03f}")
-            # Change these outputs with the longitudinal controller. Note that
-            # brake_output is optional and is not required to pass the
-            # assignment, as the car will naturally slow down over time.
+
             throttle_output = 0
             brake_output    = 0
-            forward = np.log(v_desired + 1) / 3.6
+            feed_forward = np.log(v_desired + 1) / 3.6
 
-            #acc = self.vars.acc_previous + acc_delta
-            #acc = np.clip(acc, -1.0, 1.0)
-            acc = forward + acc_delta
-
+            acc = feed_forward + acc_delta
             if acc > 0:
                 throttle_output = acc
             else:
